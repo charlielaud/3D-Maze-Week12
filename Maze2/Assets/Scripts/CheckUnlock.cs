ed64fdd79fa005c196773a926f6c86ce5b0888a6
charlielaud <charlielaud@gmail.com>
2022-09-11 13:27:21 -0700

    Added Project

---
+++ Maze2/Assets/Scripts/CheckUnlock.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckUnlock : MonoBehaviour
+{
+    public GameObject door = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        door.SetActive(true);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Key"))
+        {
+            door.SetActive(false);
+        }
+    }
+
+}
